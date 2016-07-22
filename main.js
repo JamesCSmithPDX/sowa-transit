792cb1cd3edd9d19ec3074946af3908d6a9d0f08
JamesCSmithPDX <james.c.smithpdx@gmail.com>
2016-07-22 15:14:56 -0700

    adjusted ajax call; started parsing data

--- main.js
+++ main.js
@@ -3,16 +3,30 @@ var sowaLocationId = ['13185', '12760', '12880', '12883', '12845']
 var tilikumLocationIDs = ['13732', '13711', '13728', '13733', '13601', '13602'];
 
 
-var arrivalTimes = function(locationId){
-    console.log(locationId);
+var arrivalTimes = function(locationId, index, arr) {
+
     $.ajax({
-      method: 'GET',
-      url: 'https://developer.trimet.org/ws/V1/arrivals/locIDs/' + locationId + '/appID/' + appId,
-      success: function(data, message, xhr) {
-        console.log(data);
-      }
+        method: 'GET',
+        url: 'https://developer.trimet.org/ws/V1/arrivals/json/true/streetcar/true/locIDs/' + locationId + '/appID/' + appId,
+        success: function(data, message, xhr) {
+            if (arr == sowaLocationId) {
+                getArrivals(data);
+                //getSowaTemplate();
+            } else {
+                console.log("Tilikum");
+            };
+        }
     });
-  };
+};
 
 sowaLocationId.forEach(arrivalTimes);
 tilikumLocationIDs.forEach(arrivalTimes);
+
+function logArrayElements(element, index, array) {
+  console.log('a[' + index + '] = ' + element);
+};
+
+getArrivals = function(data){
+  var arrivalArray = data.resultSet.arrival;
+  arrivalArray.forEach(logArrayElements);
+};

f38b24463231bee6976236137aa50b64c49f46bb
JamesCSmithPDX <james.c.smithpdx@gmail.com>
2016-10-01 12:09:32 -0700

    fixed sort function

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var sowaLocationId = ['13185', '12760', '12880', '12883', '13601', ]
 var tilikumLocationIDs = ['13732', '13711', '13728', '13733', '13602'];
 var sowaArrivals = [];
 var tilikumArrivals = [];
+var sortArray = [];
 
 var arrivalTimes = function(locationId, index, arr) {
 
@@ -10,7 +11,6 @@ var arrivalTimes = function(locationId, index, arr) {
         method: 'GET',
         url: 'https://developer.trimet.org/ws/V1/arrivals/json/true/streetcar/true/locIDs/' + locationId + '/appID/' + appId,
         success: function(data, message, xhr) {
-            console.log();
             if (arr == sowaLocationId) {
                 var myClass = 'sowa'
                 getArrivals(data, myClass);
@@ -55,36 +55,55 @@ function fixTime(a) {
   a.estimated = fixTime;
   };
 
-getArrivals = function(data, myClass) {
-  console.log(myClass);
+getArrivals = function(data, myClass,callback) {
   var arrivalArray = data.resultSet.arrival;
 
   if (myClass = "sowa") {
     arrivalArray.forEach(fixTime);
-    pushArray(sowaArrivals, arrivalArray);
-    console.log(sowaArrivals);
-    // arrivalArray.forEach(logArrayElements);
+    pushArray(sortArray, arrivalArray);
+    console.log(sortArray);
+    arrivalArray.forEach(logArrayElements);
 
   } else {
     arrivalArray.forEach(fixTime);
-    pushArray(tilikumArrivals, arrivalArray);
-    console.log('tilikum' + tilikumArrivals);
-    // arrivalArray.forEach(logTilikumElements);
+    pushArray(sortArray, arrivalArray);
+    console.log(sortArray);
+    arrivalArray.forEach(logTilikumElements);
   }
 };
 
-
 function pushArray(arr, arr2) {
-    arr.push.apply(arr, arr2);
+    Array.prototype.push.apply(arr, arr2);
     arr.sort(function(a,b) {
       return (new Date(b.estimated)) - (new Date(a.estimated));
   });
 };
 
-sowaLocationId.forEach(arrivalTimes);
-tilikumLocationIDs.forEach(arrivalTimes);
 
-function postData() {
-  sowaArrivals.foreach(logArrayElements);
-  tilikumArrivals.forEach(logTilikumElements);
-};
+// function postData() {
+//   sowaArrivals.foreach(logArrayElements);
+//   tilikumArrivals.forEach(logTilikumElements);
+// };
+
+async.series([
+    function(callback) {
+        sowaLocationId.forEach(arrivalTimes, callback);
+        tilikumLocationIDs.forEach(arrivalTimes, callback);
+        console.log('one');
+        callback(null, 'one');
+    },
+    function(callback) {
+
+        console.log('two');
+        callback(null, 'two');
+    },
+    function(callback) {
+        console.log(sortArray);
+        console.log('three');
+        callback(null, 'three');
+    },
+],
+// optional callback
+function(err, results) {
+    // results is now equal to ['one', 'two']
+});
